52f09512a307d3565527f6800dda0d00f6d477a4
Hariharan <109363771+hariFED@users.noreply.github.com>
2023-09-10 20:01:56 +0530

    Replaced the folder of components
    --> Moved the component folder from the assets folder as it was wrongly placed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 
-import Header from './assets/components/Header'
-import Content from './assets/components/Content'
-import Buttons from './assets/components/Buttons'
+import Header from './components/Header'
+import Content from './components/Content'
+import Buttons from './components/Buttons'
 import './App.css'
 
 function App() {
